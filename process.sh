eef44a46a98bd561e9a49915fc34e30b41331e59
tmont <tommy.mont@gmail.com>
2016-04-29 23:43:45 -0700

    run eslint and jsdoc after generating

--- process.sh
+++ process.sh
@@ -9,4 +9,30 @@ wait
 rm src/gen/resources/* src/gen/types/* src/gen/attributes/*
 node create-objects.js || exit 1
 
+errors=
+
+echo "linting generated code..."
+node_modules/.bin/eslint src/gen
+if [ $? -ne 0 ]; then
+	echo "LINT FAILED!"
+	errors="lint"
+else
+	echo " yay!"
+fi
+
+echo "making sure there are no jsdoc errors..."
+node_modules/.bin/jsdoc -X -r src > /dev/null
+if [ $? -ne 0 ]; then
+	echo "JSDOC FAILED!"
+	errors="${errors} jsdoc"
+else
+	echo " yay!"
+fi
+
 echo "finished in ${SECONDS}s"
+
+if [ -n "${errors}" ]; then
+	echo "there were some errors, please investigate: ${errors}"
+	exit 1
+fi
+
